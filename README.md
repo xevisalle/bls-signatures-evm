a7d0e7fb23f672bf2b672f9cc42142f05e903e98
xevisalle <xavier@dusk.network>
2025-11-11 17:38:20 +0100

    Add EVM serialization support

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # BLS signatures in Rust
 
-This repository includes the implementation of BLS signatures in Rust, using the BLS12-381 elliptic curve. It uses keccak256 as a hashing function, to have maximum optimization for verifications on EVMs.
+This repository includes the implementation of BLS signatures in Rust, using the BLS12-381 elliptic curve. It uses keccak256 as a hashing function, to have maximum optimization for verifications on EVMs. Plus, it offers an EVM-specific serialization, compatible with the BLS12-381 precompiles. 
+
+For a BLS signature verification in Solidity compatible with this implementation, check out [this library](https://github.com/xevisalle/solidity-ecc).
 
 **DISCLAIMER:** the code in this repository has NOT went through an exhaustive security review. Use at your own risk.
 
@@ -44,6 +46,25 @@ assert!(aggr_sig_diff.verify_aggregated_diff_msg(
     &[&msg, &msg_diff],
     &[pk, pk2]
 ));
+```
+
+## Serialization for EVM BLS12-381 precompiles
+
+We implement serialization methods for both `Signature` and `PublicKey` compatible with the EVM BLS12-381 precompiles, that saves us some gas since they don't have to be formatted in-contract. Check out the following example:
+
+```rust
+use bls_signatures_evm::*;
+use rand::rngs::OsRng;
+
+let sk = SecretKey::random(&mut OsRng);
+let pk = PublicKey::from(&sk);
+
+let msg = 1234u64.to_be_bytes();
+let sig = sk.sign(&msg);
+
+// We serialize both the public key and the signature
+let serialized_pk: [u8; 256] = pk.to_evm_bytes();
+let serialized_sig: [u8; 128] = sig.to_evm_bytes();
 ```
 
 ## Benchmarks
--- src/keys.rs
+++ src/keys.rs
@@ -15,7 +15,7 @@ impl SecretKey {
 
     /// Produces a BLS signature of a message.
     ///
-    /// IMPORTANT: it hashes the message using [`truncated_hash()`], which drops
+    /// IMPORTANT: it hashes the message using truncated_hash(), which drops
     /// 2 bits from the hashed value before mapping to the group. This reduces the
     /// security bits of the scheme.
     pub fn sign(&self, message: &[u8]) -> Signature {
@@ -51,6 +51,36 @@ impl PublicKey {
     /// Deserializes a byte representation in compressed form into a public key.
     pub fn from_bytes(bytes: &[u8; 96]) -> Result<Self, Error> {
         let point = G2Affine::from_compressed(bytes);
+        match point.is_some().unwrap_u8() {
+            1 => Ok(PublicKey(point.unwrap())),
+            _ => Err(Error::InvalidBytes),
+        }
+    }
+
+    /// Serializes the public key into a byte representation in uncompressed form
+    /// suitable for its usage with EVM precompiles.
+    pub fn to_evm_bytes(&self) -> [u8; 256] {
+        let bytes = self.0.to_uncompressed();
+        let mut evm_bytes = [0u8; 256];
+
+        evm_bytes[16..64].copy_from_slice(&bytes[48..96]);
+        evm_bytes[80..128].copy_from_slice(&bytes[..48]);
+        evm_bytes[144..192].copy_from_slice(&bytes[144..]);
+        evm_bytes[208..].copy_from_slice(&bytes[96..144]);
+
+        evm_bytes
+    }
+
+    /// Deserializes a byte representation in EVM-style uncompressed form into a public key.
+    pub fn from_evm_bytes(bytes: &[u8; 256]) -> Result<Self, Error> {
+        let mut point_bytes = [0u8; 192];
+
+        point_bytes[48..96].copy_from_slice(&bytes[16..64]);
+        point_bytes[..48].copy_from_slice(&bytes[80..128]);
+        point_bytes[144..].copy_from_slice(&bytes[144..192]);
+        point_bytes[96..144].copy_from_slice(&bytes[208..]);
+
+        let point = G2Affine::from_uncompressed(&point_bytes);
         match point.is_some().unwrap_u8() {
             1 => Ok(PublicKey(point.unwrap())),
             _ => Err(Error::InvalidBytes),
